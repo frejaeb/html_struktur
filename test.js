428f797a8c3c005784be765e7e037ed1b5006e7f
Freja <frejaeb@gmail.com>
2023-08-24 10:33:50 +0200

    js consol5

---
+++ test.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+console.log(header);
+
+const billed = document.querySelector("img");
+console.log(billed);
+
+const article = document.querySelector("article");
+console.log(article);
+
+const brødtekst = document.querySelector("p");
+console.log(brødtekst);
+
+const footer = document.querySelector("footer");
+console.log(footer);
